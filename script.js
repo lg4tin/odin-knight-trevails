59531d5465dcf03f744b62d0acb98459a50ca872
lg4tin <lg4tin@gmail.com>
2023-11-07 21:01:10 -0500

    Add isInsideBoard function

--- script.js
+++ script.js
@@ -76,35 +76,6 @@ class ChessBoard {
 
 const chessBoard1 = new ChessBoard()
 
-
-/////Create adjacency list for chessboard
-
-class Chess { 
-  constructor() { 
-    this.numberOfNodes = 0;
-    this.adjacentList = {
-    }; 
-  } 
-  addVertex(node)  {
-    this.adjacentList[node] = [];
-    this.numberOfNodes++;
-  } 
-  addEdge(node1, node2) { 
-    this.adjacentList[node1].push(node2)
-    this.adjacentList[node1].sort();
-    this.adjacentList[node2].push(node1)
-    this.adjacentList[node2].sort();
-  } 
-  showConnections() { 
-    const allNodes = Object.keys(this.adjacentList); 
-    for (let node of allNodes) { 
-      let nodeConnections = this.adjacentList[node]; 
-      let connections = ""; 
-      let vertex;
-      for (vertex of nodeConnections) {
-        connections += vertex + " ";
-      } 
-      console.log(node + "-->" + connections); 
-    } 
-} 
-} 
+function isInsideBoard(x, y) {
+  return x >= 0 && x <= 8 && y >= 0 && y <= 8;
+}
